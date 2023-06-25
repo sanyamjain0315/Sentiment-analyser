77deb9a3ddc325dc55ea730d853b4c136a8b5167
sanyamjain0315 <111227515+sanyamjain0315@users.noreply.github.com>
2023-06-25 21:24:33 +0530

    [REFACTOR]: Organized code and requirements file

--- app.py
+++ app.py
@@ -1,57 +1,53 @@
 import streamlit as st
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import dotenv
-from dotenv import load_dotenv
-import os
 import requests
 
 
-# vader analyzer
-st.title('Sentiment Analysis Tool')
-st.subheader('Vader Sentiment')
-text = st.text_input('Enter a comment')
-click = st.button('Compute')
-
+# Vader sentiment(positive/negative)
 def vader_senti(text):
+    st.subheader('Vader Sentiment')
     senti = SentimentIntensityAnalyzer()
     senti_results = senti.polarity_scores(text)
-    graph_data = {"Negative":senti_results['neu']*100,"Positive":senti_results['pos']*100}
-    st.bar_chart(data=graph_data)
+    positive_results = senti_results['pos']
+    negative_results = senti_results['neu']
+    
     if senti_results['compound'] >= 0.05:
-        st.write("Positive")
+        st.write("Sentence is Positive")
     elif senti_results['compound'] <= 0.05:
-        st.write('Negative')
+        st.write('Sentence is Negative')
     else:
-        st.write('Neutral')
+        st.write('Sentence is Neutral')
 
-if click:
-    vader_senti(text)
+    st.write(f'Positivity: {positive_results}')
+    st.write(f'Negativity: {negative_results}')
 
+    graph_data = {"Positive":positive_results*1000, "Negative":negative_results*1000}
+    st.bar_chart(data=graph_data)
 
-# Hugging face model
-load_dotenv()
-st.header("Speech classification")
-API_URL = "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base"
-headers = {"Authorization": "Bearer "+st.secrets['HUGGINGFACE_API']}
-
+# Hugging face model(emotions)
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-sentence = st.text_input("Enter your text")
-button = st.button('Results')
+    API_URL = "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base"
+    headers = {"Authorization": "Bearer "+st.secrets['HUGGINGFACE_API']}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 def hugging_senti(text):
+    st.header("Speech classification")
     result = query({"inputs":text,})[0]
     data = {}
     for emotion in result:
         st.write(emotion['label'])
-        st.write(emotion['score']*100)
+        st.write(round(emotion['score']*100, 2))
 
         data[emotion['label']] = emotion['score']*100
 
     st.bar_chart(data=data)
     return result
 
-if button:
-    hugging_senti(sentence)
+
+st.title('Sentiment Analysis Tool')
+text = st.text_input('Enter a comment')
+compute = st.button('Compute')
+if compute:
+    vader_senti(text)
+    hugging_senti(text)
